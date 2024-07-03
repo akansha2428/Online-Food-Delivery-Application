71556d979c399776f2ff696cda68e19fe026b96f
akansha2428 <108613148+akansha2428@users.noreply.github.com>
2024-07-03 22:22:22 +0530

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+     <div className='header-contents'>
+      <h2>Order Your Favourite Juice Here</h2> 
+      <p>Choose from a diverse menu featuring a delectable array of dishes
+      crafted with the finest ingredients and culinary expertise. 
+      Our mission is to satisfy your cravings and elevate your dining with
+      expertise. </p> 
+      <button>View Menu</button>
+     </div>
+    </div>
+  )
+}
+
+export default Header
